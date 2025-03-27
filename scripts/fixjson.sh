e9fd93d49ee4f4ee572232af67bb561eeacd7d3a
David A Brown <dabrown645@gmail.com>
2025-03-26 23:32:32 -0700

    🐞 FIX: Did lots of fixing and additions I think is close enough to commit now

--- scripts/fixjson.sh
+++ scripts/fixjson.sh
@@ -1,6 +1,5 @@
-while read pkg ; do
+while read pkg; do
   printf "      {\"name\": \"${pkg}\", "
-  desc=$( yay -Si ${pkg} | rg Description | sed -e "s/: /:/" | cut -f2 -d ":")
+  desc=$(yay -Si ${pkg} | rg Description | sed -e "s/: /:/" | cut -f2 -d ":")
   printf "\"description\": \"${desc}\"},\n"
- done
-
+done
--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,12 +1,73 @@
+#!/usr/bin/env bash
 fix_pacmanconf() {
   pacmanconf=${1:-/etc/pacman.conf}
 
-  sed -i -e 's/#Parallel.*/ParallelDownloads = 20/' \
+  sed -i -e 's/#*Parallel.*/ParallelDownloads = 20/' \
     -e 's/#Color/Color/' \
     -e 's/#VerbosePkgLists/VerbosePkgLists/' \
     -e 's/#CheckSpace/CheckSpace/' \
-    ${pacmanconf}
-  sed -i '/Parallel/a ILoveCandy' ${pacmanconf}
+    "${pacmanconf}"
+  sed -i '/Parallel/a ILoveCandy' "${pacmanconf}"
+}
+
+fix_grub-btrfs() {
+  grub_btrfs_config=${1:-/etc/default/grub-btrfs/config}
+
+  cp "${grub_btrfs_config}" "${grub_btrfs_config}.$(date +%y%m%d)"
+  sed -i -e 's/#*GRUB_BTRFS_GRUB_DIRNAME.*/GRUB_BTRFS_GRUB_DIRNAME=\"\/efi\/grub\"/' \
+    -e 's/#*GRUB_BTRFS_GBTRFS_DIRNAME.*/GRUB_BTRFS_GBTRFS_DIRNAME=\"\/efi\/grub\"/' \
+    "${grub_btrfs_config}"
+}
+
+fix_locale_gen() {
+  local_gen=${1:-/etc/locale.gen}
+
+  sed -i -e 's/#*en_US.UTF-8/en_US.UTF-8/' "${local_gen}"
+}
+
+fix_locale_conf() {
+  locale_conf=${1:-/etc/locale.conf}
+
+  {
+    echo "LANG=en_US.UTF-8"
+    echo "LC_ADDRESS=en_US.UTF-8"
+    echo "LC_IDENTIFICATION=en_US.UTF-8"
+    echo "LC_MEASUREMENT=en_US.UTF-8"
+    echo "LC_MONETARY=en_US.UTF-8"
+    echo "LC_NAME=en_US.UTF-8"
+    echo "LC_NUMERIC=en_US.UTF-8"
+    echo "LC_PAPER=en_US.UTF-8"
+    echo "LC_TELEPHONE=en_US.UTF-8"
+    echo "LC_TIME=en_US.UTF-8"
+  } >"${locale_conf}"
+}
+
+fix_vconsole_conf() {
+  vsconsole_conf=${1:-/etc/vconsole.conf}
+  {
+    echo "FONT=ter-220n"
+    echo "KEYMAP=us"
+  } >"${vsconsole_conf}"
+}
+
+fix_hostname() {
+  hostname=${1:-/etc/hostname}
+
+  {
+    echo "archlinux"
+  } >"${hostname}"
+}
+
+fix_localtime() {
+  localtime=${1:-/etc/localtime}
+  timezone="America/Los_Angles"
+
+  ln -sf /usr/share/zoneinfo/${timezone} "${localtime}"
+}
+
+fix_sudoers_wheel() {
+  sudoers=${1:-/etc/sudoers}
+  sed -i -e 's/# *%wheel  *ALL=(ALL:ALL)  *ALL/%wheel ALL=(ALL:ALL) ALL/' "${sudoers}"
 }
 
 box_me() {
@@ -21,6 +82,7 @@ box_me() {
 }
 
 mylsblk() {
-  lsblkopt="-o NAME,FSTYPE,PARTLABEL,MOUNTPOINTS"
-  lsblk ${lsblkopt} ${1}
+  lsblkopt="NAME,FSTYPE,PARTLABEL,MOUNTPOINTS"
+  # shellcheck disable=SC2086
+  lsblk -o "${lsblkopt}" ${1}
 }
