d3167e923377ed9011af3b70178f078908c83a7e
David A Brown <dabrown645@gmail.com>
2025-04-09 16:16:24 -0700

    🐞 FIX: Additions to inventory & minor typo to fixjson.sh

--- scripts/fixjson.sh
+++ scripts/fixjson.sh
@@ -1,5 +1,5 @@
 while read pkg; do
-  printf "      {\"name\": \"${pkg}\", "
+  printf "      { \"name\": \"${pkg}\", "
   desc=$(yay -Si ${pkg} | rg Description | sed -e "s/: /:/" | cut -f2 -d ":")
   printf "\"description\": \"${desc}\"},\n"
 done
